5145239b181c586ef628f8b59abb9a80f09e17d2
Larissa Menegaldi <l.menegaldi@gmail.com>
2020-11-04 15:11:25 +0100

    Functioning button

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function generator () {
-  let healthyHabit = ['Drink Water'];
-  document.write(healthyHabit).innerHTML = healthyHabit
+function renderElement () {
+  let createElement = document.createElement('p')
+  createElement.textContent = 'New Habit'
+
+  let habitDescription = document.createElement('p')
+  habitDescription.textContent = 'Meow in empty rooms roll over and sun my belly poop in the plant pot thug cat . Cat cat moo moo lick ears lick paws destroy the blinds. So youre just gonna scroll by without saying meowdy?'
+
+  let wrapperElement = document.getElementById('wrapper')
+  wrapperElement.appendChild(createElement)
+  let wrapperElement2 = document.getElementById('wrapper2')
+  wrapperElement2.appendChild(habitDescription)
+  wrapperElement2.classList.add('habit-description')
 }
+
+// function renderElement () {
+//   let createElement = document.createElement('p')
+//   createElement.textContent = 'Description'
+  
+
+//   let wrapperElement = document.getElementById('wrapper2')
+//   wrapperElement.appendChild(createElement)
+// }
