958f7ad1a7e374bb4b24b74af03f0789f5a7adbd
Larissa Menegaldi <l.menegaldi@gmail.com>
2020-11-05 14:24:55 +0100

    completed generator

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-function renderElement () {
+function randomElement (list) {
+  const randomFloat = Math.random() * (list.length - 1)
+  const randomIndex = Math.round(randomFloat)
+  return list[randomIndex]
+}
+
+const listOfHabbits = [
+	{ title: 'my title 1', description: 'my description 1' }, 
+  { title: 'my title 2', description: 'my description 2' },
+  { title: 'my title 3', description: 'my description 3' }
+]
+
+function clickAction () {
   let pCheck = document.querySelector ('#titlebox p') 
   let pCheck2 = document.querySelector ('#descriptionbox p')
+  const randomHabbit = randomElement(listOfHabbits)
   
   if (pCheck) { 
-    pCheck.textContent = 'Another habit' 
+    pCheck.textContent = randomHabbit.title
   }
 
   if (pCheck2) { 
-    pCheck2.textContent = 'Another description' 
+    pCheck2.textContent = randomHabbit.description
   }
 
   else {
     let createElement = document.createElement('p')
-    createElement.textContent = 'New Habit'
+    createElement.textContent = randomHabbit.title
 
     let habitDescription = document.createElement('p')
-    habitDescription.textContent = 'Meow in empty rooms roll over and sun my belly poop in the plant pot thug cat . Cat cat moo moo lick ears lick paws destroy the blinds. So youre just gonna scroll by without saying meowdy?'
+    habitDescription.textContent = randomHabbit.description
 
     let wrapperElement = document.getElementById('titlebox')
     wrapperElement.appendChild(createElement)
@@ -23,38 +36,20 @@ function renderElement () {
     let wrapperElement2 = document.getElementById('descriptionbox')
     wrapperElement2.appendChild(habitDescription) 
     wrapperElement2.classList.add('habit-description')
+
+    
   }
 }
 
-// var habitTitle = new Array("Habit1", "Habit2", "Habit3");
-// var habitDescription = new Array("Description1", "Description2", "Description3");
-
-var habits = [
-  {title: "Habit1", description: "Description1"},
-  {title: "#Habit2", description: "Description2"},
-  {title: "#Habit3", description: "Description3"},
-];
+// Objects
+// Person
 
 
-$(document).ready(function() {
-  $('#clickMe').click(function(){
-  var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-    $('#title').text('Color: ' + randomQuote.title);
-    $('#description').text('Quote: ' + randomQuote.description);
+// Title styling
+// console.log('*****', randomHabbit.title, '*****')
 
-  })
-});
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<button type="button" id="clickMe">Click for a random quote</button>
-<p id ="color"></p>
-<p id ="quote"></p>
-<p id ="author"></p>
-
-
-
-
-
-
+// Description styling
+// console.log('-----', randomHabbit.description, '-----')
 
 
 // else {
