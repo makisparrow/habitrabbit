d8d1d29e7aaedcae9a0819ec95b1873c03908081
Max Wroblewski <maxwroblewski@MacBook-Pro.local>
2020-11-06 10:28:19 +0100

    CSS first fix

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ function randomElement (list) {
 }
 
 const listOfHabbits = [
-	{ title: 'my title 1', description: 'my description 1' }, 
-  { title: 'my title 2', description: 'my description 2' },
-  { title: 'my title 3', description: 'my description 3' }
+	{ title: 'Practice gratitude', description: 'It is so easy to get in the bad habit of envying what others have.  The grass often seems greener on the other side of the fence.  It is vitally important to learn gratitude.  Practice thinking about the things you have to be thankful about.  I do this with my daughter each evening when I put her to bed.  I ask her to recall the good things that happened that day.  We then take a few minutes and give thanks for those things.  This helps us both to train ourselves to be grateful.' }, 
+  { title: 'Manage money wisely', description: 'Why don’t they teach a good personal finance class in school?  Students need to learn to earn, save, budget, track, and wisely spend money to be successful in life.  Good money habits can never start too early.  There are a lot of good resources out there to help.  I recommend taking a look at what Dave Ramsey has to offer for various ages.  Of course, you can also find a lot of fantastic money tips reading blogs!' },
+  { title: 'Always tell the truth', description: 'The truth often comes out whether we want it to or not.  Lying generally just complicates the situation and makes us look bad.  Look at the scandals many of our politicians fall into because they fail to admit the truth.  It is much better to just develop the habit of telling the truth even when it is difficult.  This will save you a lot of heartache and misery in life.' },
+  { title: 'Be punctual', description: 'Arriving on time is important to one’s success.  People always notice when you are late.  It is an indicator of whether you mean what you say and can be trusted.  Do not introduce doubt about this into people’s minds by showing up later than expected.  Create the habit of being punctual now and you won’t have to worry.' },
+  { title: 'Be a regular reader', description: 'Being a good reader is a skill that often separates the good students from those that struggle.  Becoming a good reader takes practice.  The more you read and are read to, the better you get.  Reading has numerous benefits.  It builds one’s vocabulary, expands the imagination, and rekindles creativity.  Make reading a routine!' },
 ]
 
 function clickAction () {
@@ -53,7 +55,7 @@ function clickAction () {
     //Accept button
 
     let acceptChallange = document.createElement('button')
-    acceptChallange.textContent = "Accept Challange"
+    acceptChallange.textContent = "Accept this habit"
 
     let accpetElement = document.getElementById('action-div')
     accpetElement.appendChild(acceptChallange)
@@ -67,7 +69,7 @@ function clickAction () {
   // console.dir(toggleButton);
 
 if (buttonChange) {
-    toggleButton.innerText = "Generate a new habit"
+    toggleButton.innerText = "Generate another habit"
   }
 
 else {
