d564193b1ed700a8efe24f72390cdb35736d5b78
Larissa Menegaldi <l.menegaldi@gmail.com>
2020-11-12 13:38:47 +0100

    annoucement card

--- script2.js
+++ script2.js
@@ -3,11 +3,17 @@ let searchParams = new URLSearchParams(query)
 let name = searchParams.get("name")
 let title = searchParams.get("title")
 
-let text = "Congratulations " + name + ", you just commited to " + title + "."
+let text = "Well done " + name + ", \n You have committed to " + title + " and you should be proud of yourself. \nBy inviting a new habit into your life, no matter how big or small, you have taken the first step towards a healthier life. \nAccording to science, you need about two months of daily practise until " + title + " becomes a natural part of your everyday routine."
+
+
 document.querySelector('#myname').textContent = text
 
-// name.classlist.add('user-new-habit')
-// title.classlist.add('user-new.habit')
+
+  
+
+
+name.classlist.add('user-new-habit')
+title.classlist.add('user-new.habit')
 
 var confettiSettings = { target: 'my-canvas' };
 var confetti = new ConfettiGenerator(confettiSettings);
