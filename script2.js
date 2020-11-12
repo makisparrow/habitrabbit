a1305834ada7054660810785dad5f73eff2d8354
Quennie Sjöbeck <quenniesjobeck@Quennies-Air.lan>
2020-11-12 15:58:40 +0100

    Div footer

--- script2.js
+++ script2.js
@@ -7,7 +7,7 @@ let searchParams = new URLSearchParams(query)
 let name = searchParams.get("name")
 let title = searchParams.get("title")
 
-let text = "Well done " + name + ", \n\n You have committed to " + title + " and you should be proud of yourself. \n\nBy inviting a new habit into your life, no matter how big or small, you have taken the first step towards a healthier life. \n\nAccording to science, you need about two months of daily practise until " + title + " becomes a natural part of your everyday routine."
+let text = "Well done " + name + ", \n\n You have committed to " + title + " and you should be proud of yourself. \n\nBy inviting a new habit into your life, no matter how big or small, you have taken the first step towards a healthier life. According to science, you need about two months of daily practise until " + title + " becomes a natural part of your everyday routine."
 document.querySelector('#myname').textContent = text
 
 name.classlist.add('user-new-habit')
